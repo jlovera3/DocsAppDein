433bc66abd928349cad040a77a4fc9c6abde483d
jlovera3 <jloveraulecia@gmail.com>
2020-03-09 16:58:20 +0100

    docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListadoComponent } from './listado/listado.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { BuscarComponent } from './buscar/buscar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'listado', component: ListadoComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: 'buscar', component: BuscarComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoComponent,
+    FormularioComponent,
+    BuscarComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
